51b8b1b97fb3a04b46c1b195e4a6c8134658a519
Ben <bpk0814@live.com>
2018-05-02 10:39:28 -0700

    Updated with basic layouts of MapFragment and AtlasActivity

--- app/src/main/java/com/example/bkummert/atlasapp/AtlasActivity.java
+++ app/src/main/java/com/example/bkummert/atlasapp/AtlasActivity.java
@@ -1,14 +1,44 @@
 package com.example.bkummert.atlasapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.UUID;
+
 public class AtlasActivity extends AppCompatActivity {
+    private UUID mId;
+    private String mName;
+    private Image mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atlas);
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public Image getImage() {
+        return mImage;
+    }
+
+    public void setImage(Image image) {
+        mImage = image;
     }
 }
 
