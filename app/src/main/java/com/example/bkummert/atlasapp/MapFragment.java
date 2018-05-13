7bb4aa24b0bdb566d14a56c7f548268d947ad467
Vikram <vikramkher@live.com>
2018-05-12 17:55:03 -0700

    Added images to map fragment

--- app/src/main/java/com/example/bkummert/atlasapp/MapFragment.java
+++ app/src/main/java/com/example/bkummert/atlasapp/MapFragment.java
@@ -1,11 +1,13 @@
 package com.example.bkummert.atlasapp;
 
+import android.content.res.TypedArray;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.UUID;
@@ -17,6 +19,7 @@ import java.util.UUID;
 public class MapFragment extends Fragment {
     private Map mMap;
     private TextView mTextView;
+    private ImageView mImageView;
     private static final String ARG_MAP_ID = "map_id";
 
     @Override
@@ -31,7 +34,34 @@ public class MapFragment extends Fragment {
         View v = inflater.inflate(R.layout.map_fragment, container, false);
         mTextView = (TextView)v.findViewById(R.id.map_title);
         mTextView.setText(mMap.getTitle());
+        mImageView = (ImageView)v.findViewById(R.id.map_photo);
+        TypedArray imageArray = getResources().obtainTypedArray(R.array.map_images);
+        int pos = 0;
+        if(mMap.getTitle().equals("United States"))
+        {
+            pos = 0;
+        }
+        if(mMap.getTitle().equals("South America"))
+        {
+            pos = 1;
+        }
+        if(mMap.getTitle().equals("Europe"))
+        {
+            pos = 2;
+        }
+        if(mMap.getTitle().equals("Africa"))
+        {
+            pos = 3;
+        }
+        if(mMap.getTitle().equals("Asia"))
+        {
+            pos = 4;
+        }
+        mImageView.setImageResource(imageArray.getResourceId(pos,0));
+
+        imageArray.recycle();
         return v;
+
     }
 
 }
