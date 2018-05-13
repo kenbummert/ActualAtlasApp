00be3983008153ea07e42b1ca8334308fdc974b7
Ben <bpk0814@live.com>
2018-05-13 16:53:14 -0700

    searching added

--- app/src/main/java/com/example/bkummert/atlasapp/AtlasListFragment.java
+++ app/src/main/java/com/example/bkummert/atlasapp/AtlasListFragment.java
@@ -1,18 +1,25 @@
 package com.example.bkummert.atlasapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.util.SortedList;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +29,7 @@ import java.util.List;
 public class AtlasListFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private MapAdapter mMapAdapter;
+    private List<Map> mMaps;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +37,12 @@ public class AtlasListFragment extends Fragment {
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.atlas_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mMaps = new ArrayList<Map>();
+        mMaps.add(new Map("United States"));
+        mMaps.add(new Map("South America"));
+        mMaps.add(new Map("Europe"));
+        mMaps.add(new Map("Africa"));
+        mMaps.add(new Map("Asia"));
         setHasOptionsMenu(true);
         updateUI();
         return view;
@@ -38,7 +52,35 @@ public class AtlasListFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
         super.onCreateOptionsMenu(menu, menuInflater);
         menuInflater.inflate(R.menu.atlas_list_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                query = query.toLowerCase();
+
+                final List<Map> filteredModelList = new ArrayList<>();
+                for (Map model : mMaps) {
+                    final String text = model.getTitle().toLowerCase();
+                    if (text.contains(query)) {
+                        filteredModelList.add(model);
+                    }
+                }
+                mMapAdapter.animateTo(filteredModelList);
+                mRecyclerView.scrollToPosition(0);
+                return true;
+            }
+        });
     }
+
+
     private void updateUI() {
         AtlasList atlasList = AtlasList.get(getActivity());
         List<Map> maps = atlasList.getMapList();
@@ -69,8 +111,11 @@ public class AtlasListFragment extends Fragment {
         }
     }
 
-    private class MapAdapter extends RecyclerView.Adapter<MapHolder> {
+
+
+    private class MapAdapter extends RecyclerView.Adapter<MapHolder>  {
         private List<Map> mMapList;
+        private List<Map> mapHolder;
 
         public MapAdapter(List<Map> maps) {
             mMapList = maps;
@@ -83,6 +128,7 @@ public class AtlasListFragment extends Fragment {
             return new MapHolder(layoutInflater, parent);
         }
 
+
         @Override
         public void onBindViewHolder(MapHolder holder, int position) {
             Map map = mMapList.get(position);
@@ -92,6 +138,56 @@ public class AtlasListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mMapList.size();
+        }
+
+        public Map removeItem(int position) {
+            final Map model = mMapList.remove(position);
+            notifyItemRemoved(position);
+            return model;
+        }
+
+        public void addItem(int position, Map map) {
+            mMapList.add(position, map);
+            notifyItemInserted(position);
+        }
+
+        public void moveItem(int fromPosition, int toPosition) {
+            final Map model = mMapList.remove(fromPosition);
+            mMapList.add(toPosition, model);
+            notifyItemMoved(fromPosition, toPosition);
+        }
+        public void animateTo(List<Map> maps) {
+            applyAndAnimateRemovals(maps);
+            applyAndAnimateAdditions(maps);
+            applyAndAnimateMovedItems(maps);
+        }
+
+        private void applyAndAnimateRemovals(List<Map> newMaps) {
+            for (int i = mMapList.size() - 1; i >= 0; i--) {
+                final Map map = mMapList.get(i);
+                if (!newMaps.contains(map)) {
+                    removeItem(i);
+                }
+            }
+        }
+
+        private void applyAndAnimateAdditions(List<Map> newMaps) {
+            for (int i = 0, count = newMaps.size(); i < count; i++) {
+                final Map map = newMaps.get(i);
+                if (!mMapList.contains(map)) {
+                    addItem(i, map);
+                }
+            }
+        }
+
+        private void applyAndAnimateMovedItems(List<Map> newModels) {
+            for (int toPosition = newModels.size() - 1; toPosition >= 0; toPosition--) {
+                final Map model = newModels.get(toPosition);
+                final int fromPosition = mMapList.indexOf(model);
+                if (fromPosition >= 0 && fromPosition != toPosition) {
+                    moveItem(fromPosition, toPosition);
+                }
+            }
         }
 
     }
